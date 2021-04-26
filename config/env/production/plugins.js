64f62950b5ffdfe9200de825e986d5621f346430
Willian Justen de Vasconcellos <willianjustenqui@gmail.com>
2021-04-26 14:22:51 -0300

    Configure SMTP to production

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -11,4 +11,19 @@ module.exports = ({ env }) => ({
       delete: {},
     },
   },
+  email: {
+    provider: "nodemailer",
+    providerOptions: {
+      host: env("SMTP_HOST", "smtp-relay.sendinblue.com"),
+      port: env("SMTP_PORT", 587),
+      auth: {
+        user: env("SMTP_USERNAME"),
+        pass: env("SMTP_PASSWORD"),
+      },
+    },
+    settings: {
+      defaultFrom: "willianjustencursos@gmail.com",
+      defaultReplyTo: "willianjustencursos@gmail.com",
+    },
+  },
 });
